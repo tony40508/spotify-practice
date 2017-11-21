dec5e50c73c1d59c2f1bc249d8bdec85732999ef
takeu <tony40508@gmail.com>
2017-11-21 19:58:22 +0800

    Add footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(window).scroll(function (e) {
+    if ($(window).scrollTop() <= 0) {
+        $(".navbar").addClass("at_top");
+    }
+    else {
+        $(".navbar").removeClass("at_top");
+    }
+});
+
 $(document).on("click", "a", function (e) {
     e.preventDefault();
     var target = $(this).attr("href");
@@ -6,3 +15,28 @@ $(document).on("click", "a", function (e) {
         scrollTop: $(target).offset().top
     }, 500);
 });
+
+$(function () {
+    $('img.svg').each(function () {
+        var $img = $(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        $.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = $(data).find('svg');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+})
